cf4ddabb0e1ee34e24af0b3769b78a5494ec44de
Katiuska <katiuskaalicea@gmail.com>
2020-10-27 21:41:48 -0700

    create constructor for App component and created functions for handling changes in App.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      list: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>This is the TodoList component. </h2>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
